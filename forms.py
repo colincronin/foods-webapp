55e876d9c9ef6486479b6d2d2b9f797092ae5ace
colincronin <colincronin@users.noreply.github.com>
2014-05-11 17:32:34 -0700

    Added Javascript to Price Field and Updated Testform for Mobile Use

--- forms.py
+++ forms.py
@@ -6,3 +6,19 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ['purchased', 'name', 'price',]
+        labels = {
+            'price': ('$'),
+        }
+        widgets = {
+            'price': forms.TextInput(attrs={
+                'type': 'number',
+                'size': '6',
+                'step': '0.01',
+                'decimal_places': '2',
+             }),
+        }
+    def __init__(self, *args, **kwargs):
+        super(ItemForm, self).__init__(*args, **kwargs)
+        self.fields['purchased'].widget.attrs['onClick'] = "this.form.submit();"
+        self.fields['price'].widget.attrs['onBlur'] = "myFunction(this.id, this.value)"
+        self.fields['price'].widget.attrs['onKeyPress'] = "enterFunction(event, this.id, this.value)"
